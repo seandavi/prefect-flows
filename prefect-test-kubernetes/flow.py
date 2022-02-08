814fc9c02c491d73aed649b69f8f49b4b561f649
Sean Davis <seandavi@gmail.com>
2022-02-08 11:23:32 -0700

    add correct project

---
+++ prefect-test-kubernetes/flow.py
@@ -0,0 +1,27 @@
+from prefect.run_configs import KubernetesRun
+
+import prefect
+from prefect import task, Flow
+from prefect.storage import GitHub
+
+
+@task
+def hello_task():
+    logger = prefect.context.get("logger")
+    logger.info("Hello world!")
+
+with Flow("hello-flow") as flow:
+    hello_task()
+
+
+# Configure extra environment variables for this flow,
+# and set a custom image
+flow.run_config = KubernetesRun(
+    env={"SOME_VAR": "VALUE"},
+    image="prefecthq/prefect"
+)
+
+flow.storage = GitHub(repo='seandavi/prefect-flows', path='prefect-test-kubernetes/flow.py')
+
+flow.register('testing')
+
